a7f8a8d6d2cc01e0e740a87a8f349a32f4a16fcf
MohmmadNada <mohamma97dnada@gmail.com>
2023-01-24 17:45:04 +0300

    Refactor, Use Clock class

--- app.js
+++ app.js
@@ -1,46 +1,61 @@
-
-class ClockPage{
+class Clock{
     constructor(){
-        ClockPage.createClock()
-        ClockPage.activateClock()   
+        Clock.activateClock()
     }
 
-    static createClock(){
-        const containerDiv = document.createElement('div')
-        containerDiv.classList.add('container')
-        const clockNumbers = [12, 3, 6, 9]
-        clockNumbers.forEach(number => {
-            let clockNumber = document.createElement('span')
-            clockNumber.innerText = number
-            clockNumber.classList.add(`number-${number}`)
-            containerDiv.appendChild(clockNumber)
+    static activateClock() {
+        setInterval(() => {
+          let hours = document.querySelector("#hours");
+          let minutes = document.querySelector("#minutes");
+          let seconds = document.querySelector("#seconds");
+          Clock.createClockHand([seconds, minutes, hours]);
         });
-        const times = ['hours', 'minutes', 'seconds']
-        times.forEach(time => {
-            let timeItem = document.createElement('i')
-            timeItem.setAttribute('id', time)
-            containerDiv.appendChild(timeItem)
-        });
-        // Use One For loop
-        document.body.appendChild(containerDiv)
+      }
+
+  static createClockHand(time) {
+    time.forEach(element => {
+        let timeType = element.getAttribute("id");
+        let day = new Date();
+        if (timeType === "seconds") {
+            let secDegree = day.getSeconds() * 6 + 270;
+            seconds.style.transform = `rotate(${secDegree}deg)`;
+        } else if (timeType === "minutes") {
+            let minDegree = day.getMinutes() * 6 + 270;
+            minutes.style.transform = `rotate(${minDegree}deg)`;
+        } else if (timeType === "hours") {
+            let hourDegree = day.getHours() * 30 + 270;
+            hours.style.transform = `rotate(${hourDegree}deg)`;
+        }
+    });
     }
 
-    static activateClock(){
-        setInterval(
-            () => {
-              let hours = document.querySelector("#hours");
-              let minutes = document.querySelector("#minutes");
-              let seconds = document.querySelector("#seconds");
-              const day = new Date();
-              let hourDegree = day.getHours() * 30 + 270
-              let minDegree = day.getMinutes() * 6 + 270
-              let secDegree = day.getSeconds() * 6 + 270
-              hours.style.transform = `rotate(${hourDegree}deg)`
-              minutes.style.transform = `rotate(${minDegree}deg)`
-              seconds.style.transform = `rotate(${secDegree}deg)`
-            }
-          )
-    }
 }
 
-new ClockPage()
+class ClockPage {
+  constructor() {
+    ClockPage.createClockItems();
+    new Clock();
+  }
+
+  static createClockItems() {
+    const containerDiv = document.createElement("div");
+    containerDiv.classList.add("container");
+    const clockNumbers = [12, 3, 6, 9];
+    clockNumbers.forEach((number) => {
+      let clockNumber = document.createElement("span");
+      clockNumber.innerText = number;
+      clockNumber.classList.add(`number-${number}`);
+      containerDiv.appendChild(clockNumber);
+    });
+    const times = ["hours", "minutes", "seconds"];
+    times.forEach((time) => {
+      let timeItem = document.createElement("i");
+      timeItem.setAttribute("id", time);
+      containerDiv.appendChild(timeItem);
+    });
+    document.body.appendChild(containerDiv);
+  }
+
+}
+
+new ClockPage();
